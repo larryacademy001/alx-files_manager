affe951742e7b28c9ddef6d2de3c4ffcecab55f7
Olanrewaju Awoyemi <olanrewajuawoyemi@gmail.com>
2024-10-03 04:22:11 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x04. Files manager
+# 0x04. Files manager
--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 import express from 'express';
-import router from './routes/index';
-import unmatchedRouteHandler from './middleware/unmatched';
-import errorHandler from './middleware/error';
-import shutdown from './utils/shutdown';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
-// Express server
-const app = express();
-const PORT = process.env.PORT || 5000;
+const server = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-app.use(unmatchedRouteHandler);
-app.use(errorHandler);
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-const server = app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
-});
-
-// Graceful shutdown
-const handler = () => shutdown(server);
-process.on('SIGINT', handler);
-process.on('SIGTERM', handler);
-process.on('SIGQUIT', handler);
-
-export default app;
+export default server;
